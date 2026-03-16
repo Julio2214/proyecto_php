588862c15c603565ef790a9c84ce6b27f358d7b6
Julio Ortiz <0710577@galileo.edu>
2026-03-16 02:37:39 +0000

    agregamos codigo e imagen

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Julio Ortiz</title>
+</head>
+<body>
+
+<?php
+echo "<h1>Hola Mundo, soy Julio Alberto Ortiz Pineda</h1>";
+echo "<img src='/Proyecto_php/mundo 1.png' alt='Mi imagen' width='300'>";
+?>
+
+</body>
+</html>
